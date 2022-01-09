4b99fccee259a2cd1421c9fb823d8e3b2a45cce6
sarangsbabu367 <48784283+sarangsbabu367@users.noreply.github.com>
2022-01-09 19:10:22 +0530

    Time: 946 ms (61.76%), Space: 17.6 MB (35.60%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -4,6 +4,7 @@ class Solution:
         
         nums.sort()
         for pos, num in enumerate(nums):
+            # Skipping the same starting number.
             if pos > 0 and nums[pos - 1] == num:
                 continue
             
@@ -18,6 +19,7 @@ class Solution:
                     triplets.append([num, nums[left], nums[right]])
                     left += 1
                     right -= 1
+                    # Skipping already found left & right pairs.
                     while left < right and nums[left - 1] == nums[left]:
                         left += 1
                     while left < right and nums[right + 1] == nums[right]:
